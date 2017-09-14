83360380e60a2bbaa067e1c05836bf214392785d
Pierre Ynard <linkfanel@yahoo.fr>
2017-09-14 05:25:07 +0200

    Rank Kobe receiver up from 3rd to 2nd for Pacific area
    Better reception, also travelx.org is slightly out of tune

--- receivers.js
+++ receivers.js
@@ -24,8 +24,8 @@ var receivers = {
 		],
 		p: [ // Pacific
 			'http://kiwisdr.hirokinet.com:8074/', // Japan
-			'http://travelx.org:8073/', // Japan
 			'http://126.61.226.194:8073/', // Japan
+			'http://travelx.org:8073/', // Japan
 			'http://24.17.196.180:8073/', // Seattle, WA
 			'http://kiwisdr.ece.uvic.ca:8073/', // Victoria, BC
 			'http://119.224.45.205:8073/', // New Zealand
